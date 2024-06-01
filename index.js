42ea637a0d88746d1328b7472b5139ddbe7c5e05
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-06-02 05:11:25 +0530

    Add: new routes

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const middleware = require('./src/middleware');
 const admin = require('firebase-admin');
 const todoRoutes = require('./src/routes/todos');
-const authRoutes = require('./src/routes/auth');
-const adminRoutes = require('./src/routes/admin');
+const authRoutes = require('./src/routes/User/auth');
+const adminRoutes = require('./src/routes/Admin/admin');
 const test = require('./src/routes/test');
-const userRoutes = require('./src/routes/User');
-
+const AdminuserRoutes = require('./src/routes/Admin/User');
+const ContactRoute = require('./src/routes/User/contact');
+const UserRoutes = require('./src/routes/User/User');
+const EventStudentRoutes = require('./src/routes/User/Event-Student');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,9 +23,13 @@ app.get('/', (req, res) => {
 );
 // app.use('/test', test)
 app.use('/api/todos', todoRoutes);
-app.use('/api', authRoutes);
-app.use('/api/admin', middleware.decodeToken, middleware.checkRole('admin'), adminRoutes);
-app.use('/api', middleware.decodeToken, middleware.checkRole('admin'), userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/contact', ContactRoute);
+// app.use('/api', UserRoutes);
+app.use('/api/admin/test', middleware.decodeToken, middleware.checkRole('admin'), adminRoutes);
+app.use('/api/admin', middleware.decodeToken, middleware.checkRole('admin'), AdminuserRoutes);
+app.use('/api', middleware.decodeToken, middleware.checkRole('user'), UserRoutes);
+app.use('/api/user', middleware.decodeToken, middleware.checkRole('user'), EventStudentRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
